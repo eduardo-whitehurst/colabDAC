0a6658dac4ac95cb7e3c39e8668d7b95380973a5
AntLacerda <lacerdaneto99@hotmail.com>
2024-03-25 11:17:52 -0300

    Criando arquivo de conexao

---
+++ src/main/java/br/com/colabdac/colabdac/conexao/Factory.java
@@ -0,0 +1,17 @@
+package br.com.colabdac.colabdac.conexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Factory {
+    public Connection getConnection() throws ClassNotFoundException {
+        try {
+            Class.forName("org.postgresql.Driver");
+            return DriverManager.getConnection("jdbc:postgresql://localhost:5432/teste", "postgres", "postgres");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
